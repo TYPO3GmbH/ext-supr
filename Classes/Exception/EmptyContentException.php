35291e5c65e4be44b85459d81fe6940ef674ba6b
Andreas Fernandez <andreas.fernandez@wmdb.de>
2018-02-13 12:04:23 +0100

    [TASK] Handle and convert exception for invalid credentails

---
+++ Classes/Exception/EmptyContentException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+namespace Supr\Supr\Exception;
+
+use TYPO3\CMS\Core\Exception;
+
+/**
+ * Exception thrown if content of response is empty
+ */
+class EmptyContentException extends Exception
+{
+}
---
+++ Classes/Exception/InvalidCredentialsException.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+namespace Supr\Supr\Exception;
+
+use TYPO3\CMS\Core\Exception;
+
+/**
+ * Exception thrown if the credentials are invalid
+ */
+class InvalidCredentialsException extends Exception
+{
+}
